ca78eba2300cf0ec327ac311ceb3a0e2c828a17c
VTPartner <vtpartner7@gmail.com>
2024-10-29 16:22:22 +0530

    s

--- vtpartner_backend/settings.py
+++ vtpartner_backend/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'mobile_app',
     'dashboard_app',
     'website_app'
@@ -50,6 +51,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "https://www.vtpartner.org",
+    "https://vtpartner.org",
 ]
 
 ROOT_URLCONF = 'vtpartner_backend.urls'
