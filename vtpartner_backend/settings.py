54843df88e86217b007bf4e6986492c94733227f
VTPartner <vtpartner7@gmail.com>
2024-10-29 15:26:11 +0530

    g

--- vtpartner_backend/settings.py
+++ vtpartner_backend/settings.py
@@ -130,3 +130,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = 'http://vtpartner.org/media/'
 MEDIA_ROOT = '/var/www/vtpartner/media/'
+#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
