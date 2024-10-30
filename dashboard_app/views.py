395390cafa4eba869412a4db7b63dd49dadb0355
VTPartner <vtpartner7@gmail.com>
2024-10-30 15:56:47 +0530

    s

--- dashboard_app/views.py
+++ dashboard_app/views.py
@@ -722,8 +722,7 @@ def add_service(request):
 @csrf_exempt  # Disable CSRF protection for this view
 def edit_service(request):
     if request.method == "POST":
-        data = json.loads(request.body)  # Parse JSON request body
-        print("data::",data)
+        
         try:
             data = json.loads(request.body)  # Parse JSON request body
             print("data::",data)
@@ -751,8 +750,8 @@ def edit_service(request):
                 print(f"Missing required fields: {', '.join(missing_fields)}")
                 return JsonResponse({"message": f"Missing required fields: {', '.join(missing_fields)}"}, status=400)
 
-            # if category_image:
-            #     category_image = upload_images2(category_image)
+            if category_image:
+                category_image = upload_images2(category_image)
             # Validate to avoid duplication
             query_duplicate_check = """
                 SELECT COUNT(*) FROM vtpartner.categorytbl WHERE category_name ILIKE %s AND category_id != %s
