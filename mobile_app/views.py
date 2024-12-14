8ab1bf46e1efa9d592c3af7b5441653367574848
VTPartner <vtpartner7@gmail.com>
2024-12-14 15:09:18 +0530

    g

--- mobile_app/views.py
+++ mobile_app/views.py
@@ -3330,11 +3330,10 @@ def new_goods_driver_recharge(request):
             try:
                 if negative_points > 0 or isExpired:
                     query = """
-                    update vtpartner.goods_driver_topup_recharge_current_points_tbl set recharge_id=%s,amount=%s,allotted_points=%s,valid_till_date=%s,payment_method=%s,payment_id=%s,remaining_points='0',negative_points='0' where topup_id=%s and driver_id=%s
+                    update vtpartner.goods_driver_topup_recharge_current_points_tbl set recharge_id=%s,allotted_points=%s,valid_till_date=%s,payment_method=%s,payment_id=%s,remaining_points='0',negative_points='0',used_points='0' where topup_id=%s and driver_id=%s
                     """
                     values = [
                             recharge_id,
-                            amount,
                             allotted_points,
                             valid_till_date,
                             payment_method,
